cb24ecf4f6f0ea46b7dbb7be07cc78fc76a41ed4
bum2us <qka342@gmail.com>
2022-08-17 17:06:13 +0900

    db tutorial

--- mysql/sql/albums.sql
+++ mysql/sql/albums.sql
@@ -1,15 +1,46 @@
 use maplestory;
 
-SELECT
-	a.seq
-    ,a.groupName
-	,b.codeKey
-    ,b.codeName
-FROM maplestory.ccg a
-JOIN maplestory.cc b ON b.groupSeq = a.seq;
+-- 공통코드
+-- SELECT
+-- 	a.seq
+--     ,a.groupName
+-- 	,b.codeKey
+--     ,b.codeName
+-- FROM maplestory.ccg a
+-- JOIN maplestory.cc b ON b.groupSeq = a.seq;
 
 -- 로그인
 -- SELECT
 -- *
 -- FROM member
 -- WHERE Id="shinbeomsu" and password ="weq211";
+
+-- 메인화면 (좋아요수가 높은 상위 24개의 사진 노출)
+-- SELECT
+-- postId
+-- ,count(*) AS userLike
+-- FROM maplestory.like
+-- GROUP BY postId
+-- ORDER BY count(*) DESC
+-- LIMIT 24
+-- ;
+
+-- 게시물 화면 ( post , image , 작성자 정보 )
+-- SELECT
+-- *
+-- FROM post a
+-- JOIN image b
+-- ON b.postId = a.seq
+-- JOIN member c
+-- ON a.writer = c.seq
+-- WHERE a.seq = 1
+-- ;
+
+-- 북마크 (사용자 seq를 통해 최신 북마크가 먼저 나오도록 정렬)
+-- SELECT
+-- postId
+-- ,addDate
+-- FROM maplestory.bookmark
+-- WHERE memberId = 3
+-- ORDER BY addDate DESC;
+
